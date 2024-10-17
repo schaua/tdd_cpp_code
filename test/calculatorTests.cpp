94ad01d69e24014383757b5bd02649add489ff10
August Schau <schaua@augustschaua.com>
2024-10-17 11:12:35 -0400

    Using GTest repository reference

--- test/calculatorTests.cpp
+++ test/calculatorTests.cpp
@@ -1,12 +1,22 @@
 #include "../include/calculator.h"
+#include <gtest/gtest.h>
 
-void given_two_positive_integers_when_asked_to_add_returns_their_sum()
+// void given_two_positive_integers_when_asked_to_add_returns_their_sum()
+// {
+//     Calculator hp16c = Calculator {};
+//     int a = 1;
+//     int b = 2;
+//     int expected = a + b;
+//     int actual = hp16c.Add(a,b);
+//     if (expected != actual) throw expected;
+    
+// }
+TEST(CalcTest, given_two_positive_integers_when_asked_to_add_returns_their_sum)
 {
     Calculator hp16c = Calculator {};
     int a = 1;
     int b = 2;
-    int expected = a + b;
-    int actual = hp16c.Add(a,b);
-    if (expected != actual) throw expected;
-    
+    int expected{a + b};
+    EXPECT_EQ(hp16c.Add(a,b), expected);
+
 }
