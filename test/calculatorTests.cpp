2f34a41ab89a506c6db30daacf194f64448cf534
August Schau <schaua@augustschaua.com>
2024-10-22 13:02:32 -0400

    Refactored tests to single parameterized for CalculateInterest.

--- test/calculatorTests.cpp
+++ test/calculatorTests.cpp
@@ -1,81 +1,42 @@
 #include "../include/calculator.h"
 #include <gtest/gtest.h>
 
-// void given_two_positive_integers_when_asked_to_add_returns_their_sum()
-// {
-//     Calculator hp16c = Calculator {};
-//     int a = 1;
-//     int b = 2;
-//     int expected = a + b;
-//     int actual = hp16c.Add(a,b);
-//     if (expected != actual) throw expected;
-    
-// }
-TEST(CalcTest, given_two_positive_integers_when_asked_to_add_returns_their_sum)
+struct InterestTestParams 
 {
-    Calculator hp16c = Calculator {};
-    int a = 1;
-    int b = 2;
-    int expected{a + b};
-    EXPECT_EQ(hp16c.Add(a,b), expected);
+    double principal;
+    double rate;
+    int time;
+    double expected;
+};
 
-}
-TEST(CalcTest, given_1000_at_5_percent_for_1_year_interest_earned_is_50)
+class InteresteCalculatorTest : public testing::TestWithParam<InterestTestParams>
 {
-    Calculator hp16c;
-    double principal{1000.0};
-    double rate{5.0}; // 5%
-    int time{1};
-    double expectedInterest{50.0};
-    double actualInterest = hp16c.CalculateInterest(principal, rate, time);
-    EXPECT_EQ(actualInterest, expectedInterest);
+    protected:
+    std::unique_ptr<Calculator> hp16c;
+
+    void SetUp() override
+    {
+        hp16c = std::make_unique<Calculator>();
+        std::cout << "Setup Called" << std::endl;
+    }
+};
+
+TEST_P(InteresteCalculatorTest, CalculateInterest)
+{
+    InterestTestParams params = GetParam();
+    EXPECT_DOUBLE_EQ(hp16c->CalculateInterest(params.principal, params.rate, params.time), params.expected);
 }
 
-TEST(CalcTest, given_1000_at_10_percent_for_1_year_interest_earned_is_100)
-{
-    Calculator hp16c;
-    double principal{1000.0};
-    double rate{10.0}; // 10%
-    int time{1};
-    double expectedInterest{100.0};
-    double actualInterest = hp16c.CalculateInterest(principal, rate, time);
-    EXPECT_EQ(actualInterest, expectedInterest);
-}
-
-TEST(CalcTest, given_1000_at_5_percent_for_2_year_interest_earned_is_50)
-{
-    Calculator hp16c;
-    double principal{1000.0};
-    double rate{5.0}; // 5%
-    int time{2};
-    double expectedInterest{100.0};
-    double actualInterest = hp16c.CalculateInterest(principal, rate, time);
-    EXPECT_EQ(actualInterest, expectedInterest);
-}
-
-TEST(CalcTest, given_0_at_5_percent_for_1_year__interest_earned_is_0)
-{
-    Calculator hp16c;
-    double principal{0};
-    double rate{5.0}; // 5%
-    int time{1};
-    double expectedInterest{0.0};
-    double actualInterest = hp16c.CalculateInterest(principal, rate, time);
-    EXPECT_EQ(actualInterest, expectedInterest);
-}
-
-TEST(CalcTest, given_1000_at_5_percent_for_0_year__interest_earned_is_0)
-{
-    Calculator hp16c;
-    double principal{1000.0};
-    double rate{5.0}; // 5%
-    int time{0};
-    double expectedInterest{0.0};
-    double actualInterest = hp16c.CalculateInterest(principal, rate, time);
-    EXPECT_EQ(actualInterest, expectedInterest);
-}
-
-// and more tests to triagulate in on the desired behavior of this function
-// negative rate
-// large principle
-// others...
+INSTANTIATE_TEST_SUITE_P(
+    InterestTests,
+    InteresteCalculatorTest,
+    ::testing::Values(
+        InterestTestParams{1000,5,1,50.0},
+        InterestTestParams{1000,10,1,100.0},
+        InterestTestParams{1000,5,2,100.0},
+        InterestTestParams{0,5,1,0.0},
+        InterestTestParams{1000,5,0,0.0},
+        InterestTestParams{1000,-5,1,-50.0},
+        InterestTestParams{1000000,7.5,5,375000.0}
+    )
+);
