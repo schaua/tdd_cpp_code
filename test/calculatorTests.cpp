7fde662b6c5d6a16a2da0a3a8f426f99bcebb055
August Schau <schaua@augustschaua.com>
2024-10-23 14:19:55 -0400

    Manually added custom double service

--- test/calculatorTests.cpp
+++ test/calculatorTests.cpp
@@ -1,5 +1,7 @@
-#include "../include/calculator.h"
 #include <gtest/gtest.h>
+#include "../include/calculator.h"
+#include "../include/rateservice.h"
+#include "../include/interestrateservice.h"
 
 struct InterestTestParams 
 {
@@ -44,4 +46,40 @@ INSTANTIATE_TEST_SUITE_P(
 TEST_F(InteresteCalculatorTest, NegativeTimeThrowsException)
 {
     EXPECT_THROW(hp16c->CalculateInterest(1000,5,-1), std::invalid_argument);
+}
+
+TEST_F(InteresteCalculatorTest, CalculateInterestAtSavingsRateOf5)
+{
+    // Arrange
+    std::string accountType{"Savings"};
+    std::string bankURL{"https://mybank.com"};
+    double principal{1000.0};
+    double rate{}; // to be obtained from the service
+    int time{1};
+    double expected{50.0};
+    std::unique_ptr<RateService> interestRateService = std::make_unique<InterestRateService>(bankURL);
+
+    // Act
+    rate = interestRateService->GetCurrentRate(accountType);
+
+    // Assert
+    EXPECT_DOUBLE_EQ(hp16c->CalculateInterest(principal, rate, time), expected);
+}
+
+TEST_F(InteresteCalculatorTest, CalculateInterestAtCheckingRateOf2)
+{
+        // Arrange
+    std::string accountType{"Checking"};
+    std::string bankURL{"https://mybank.com"};
+    double principal{1000.0};
+    double rate{}; // to be obtained from the service
+    int time{1};
+    double expected{20.0};
+    std::unique_ptr<RateService> interestRateService = std::make_unique<InterestRateService>(bankURL);
+
+    // Act
+    rate = interestRateService->GetCurrentRate(accountType);
+
+    // Assert
+    EXPECT_DOUBLE_EQ(hp16c->CalculateInterest(principal, rate, time), expected);
 }
