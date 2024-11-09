4345b988980880a3e2da6e1d12f78e72202c25d1
schaua <schaua@augustschau.com>
2024-11-09 15:25:05 -0500

    Completed Lab 02

--- test/portfoliotests.cpp
+++ test/portfoliotests.cpp
@@ -1,11 +1,21 @@
 #include <gtest/gtest.h>
 #include <portfolio.h>
 
-TEST(PortfolioTest, Given_a_new_portfolio_stock_count_is_zero)
+class PortfolioTest : public testing::Test
+{
+protected:
+        int owner_id{100};
+        Portfolio portfolio{owner_id};
+public:
+    void SetUp() override
+    {
+        std::cout << "PortfolioTest::SetUp" << std::endl;
+    }
+};
+
+TEST_F(PortfolioTest, Given_a_new_portfolio_stock_count_is_zero)
 {
     // Arrange
-    int owner_id{100};
-    Portfolio portfolio{owner_id};
     int expected{0};
 
     // Act
@@ -15,30 +25,25 @@ TEST(PortfolioTest, Given_a_new_portfolio_stock_count_is_zero)
     EXPECT_EQ(actual, expected);
 }
 
-TEST(PortfolioTest, Given_a_new_portfolio_get_owner_id_confirm_it_has_been_set)
+TEST_F(PortfolioTest, Given_a_new_portfolio_get_owner_id_confirm_it_has_been_set)
 {
     // Arrange
-    int owner_id{100};
-    Portfolio portfolio{owner_id};
 
     // Act
     auto actual{portfolio.GetOwnerId()};
 
     // Assert
     EXPECT_EQ(actual, owner_id);
-
 }
 
-TEST(PortfolioTest, Given_a_new_portfolio_the_purchase_of_10_IBM_should_return_stock_count_of_10_for_IBM)
+TEST_F(PortfolioTest, Given_a_new_portfolio_the_purchase_of_10_IBM_should_return_stock_count_of_10_for_IBM)
 {
-        // Arrange
-    int owner_id{100};
-    Portfolio portfolio{owner_id};
+    // Arrange
     int expected{10};
 
     // Act
     portfolio.Buy(10, "IBM");
-    int actual{portfolio.GetStockCount("IBM")}; 
+    int actual{portfolio.GetStockCount("IBM")};
 
     // Assert
     EXPECT_EQ(actual, expected);
