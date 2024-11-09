40e6329886b7cfbd174607d7c075700316165d89
schaua <schaua@augustschau.com>
2024-11-09 15:30:52 -0500

    Correct code for Lab03

--- src/portfolio.cpp
+++ src/portfolio.cpp
@@ -1,11 +1,23 @@
 #include <string>
 #include <portfolio.h>
+#include <stdexcept>
+#include <numeric>
 
-int Portfolio::GetStockCount(const std::string& symbol) const
+int Portfolio::GetStockCount(const std::string &symbol) const
 {
+    int total_quantity{0};
+
     if (holdings_.contains(symbol))
-        return holdings_.at(symbol);
-    return 0;
+        total_quantity =  holdings_.at(symbol);
+
+    if (symbol == "*")
+        total_quantity = std::accumulate(holdings_.begin(), holdings_.end(), 0,
+            [](const int prev_sum, const std::pair<std::string, int> &entry)
+            {
+                return prev_sum + entry.second;
+            });
+
+    return total_quantity;
 }
 
 int Portfolio::GetOwnerId() const
@@ -13,8 +25,11 @@ int Portfolio::GetOwnerId() const
     return owner_id_;
 }
 
-void Portfolio::Buy(const int quantity, const std::string& symbol)
+void Portfolio::Buy(const int quantity, const std::string &symbol)
 {
+    if (quantity < 0)
+        throw std::invalid_argument("quantity cannot be negative");
+
     if (holdings_.contains(symbol))
     {
         holdings_[symbol] += quantity;
--- test/portfoliotests.cpp
+++ test/portfoliotests.cpp
@@ -48,3 +48,38 @@ TEST_F(PortfolioTest, Given_a_new_portfolio_the_purchase_of_10_IBM_should_return
     // Assert
     EXPECT_EQ(actual, expected);
 }
+
+TEST_F(PortfolioTest, Given_a_new_portfolio_the_purchase_of_10_IBM_should_return_stock_count_of_0_for_GOOGL)
+{
+    // Arrange
+    int expected{0};
+
+    // Act
+    portfolio.Buy(10, "IBM");
+    int actual{portfolio.GetStockCount("GOOGL")};
+
+    // Assert
+    EXPECT_EQ(actual, expected);
+}
+
+TEST_F(PortfolioTest, Given_a_new_portfolio_the_purchase_of_10_IBM_should_return_stock_count_of_10_for_a_total)
+{
+    // Arrange
+    int expected{10};
+
+    // Act
+    portfolio.Buy(expected, "IBM");
+    int actual{portfolio.GetStockCount("*")};
+
+    // Assert
+    EXPECT_EQ(actual, expected);
+}
+
+
+TEST_F(PortfolioTest, Given_a_portfolio_the_purchase_of_negative_quantity_throws_an_exception)
+{
+    // Arrange
+    // Act
+    // Assert 
+    EXPECT_THROW(portfolio.Buy(-1, "IBM"), std::invalid_argument);
+}
